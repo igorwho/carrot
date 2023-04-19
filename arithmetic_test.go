a9f818dad3cc3679b32683e57ce594f7e5142c6a
Igor Felix <igor.felix@loggi.com>
2023-04-19 16:30:05 -0300

    Update test

--- arithmetic_test.go
+++ arithmetic_test.go
@@ -5,6 +5,6 @@ import "testing"
 func TestSum(t *testing.T) {
 	sum := Sum(2, 3)
 	if sum != 5 {
-		t.Errorf("Sum was incorrect, got: %d, expected: %d.", sum, 5)
+		t.Errorf("Sum was incorrect, got: %d, expected: %d. Fix the function!", sum, 5)
 	}
 }
